82cf599a2c8f97eab7a60961b780f9cc225eb8a5
Vitor <vitorantunes2003@gmail.com>
2025-04-17 22:08:19 -0300

    refactor: update BrandPage component to improve parameter handling and code structure

--- src/app/dashboard/brands/[id]/page.tsx
+++ src/app/dashboard/brands/[id]/page.tsx
@@ -17,58 +17,61 @@ type Product = {
 export default async function BrandPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-  const { brand, products } = await getBrand(params.id);
+  const { id } = await params;
+  const { brand, products } = await getBrand(id);
 
   return (
     <div className="p-6">
       <div className="flex items-center gap-2 mb-6">
-        <Link
-          href="../brands"
-          className="p-1 rounded"
-        >
+        <Link href="../brands" className="p-1 rounded">
           <ArrowLeft className="h-5 w-5" />
           <span className="sr-only">Voltar</span>
         </Link>
 
         <h1 className="text-3xl font-bold">{brand.name}</h1>
       </div>
-      <div className="flex flex-wrap gap-4">
-        {products.map((product: Product) => (
-          <div
-            key={product.id}
-            className="w-40 flex flex-col items-center hover:scale-105 transition"
-          >
-            <h2 className="text-center text-base font-semibold mb-1">
-              {product.name}
-            </h2>
 
-            <div className="relative w-full aspect-square">
-              <Image
-                src={`${process.env.NEXT_PUBLIC_IMAGE_API_URL}${product.image}`}
-                alt={product.name}
-                fill
-                sizes="100%"
-                className="object-cover rounded-lg"
-                priority
-              />
-            </div>
+      {products.length === 0 ? (
+        <p className="flex items-center justify-center text-center font-extrabold">Não há produtos cadastrados</p>
+      ) : (
+        <div className="flex flex-wrap gap-4">
+          {products.map((product: Product) => (
+            <div
+              key={product.id}
+              className="w-40 flex flex-col items-center hover:scale-105 transition"
+            >
+              <h2 className="text-center text-base font-semibold mb-1">
+                {product.name}
+              </h2>
 
-            <div className="mt-2 text-center space-y-1">
-              <p className="text-xs bg-yellow-300 inline-block px-1 rounded">
-                {product.code}
-              </p>
-              <p className="text-xs text-red-500 truncate">
-                {product.description}
-              </p>
-              <p className="font-medium bg-yellow-300 inline-block px-1 rounded">
-                R$ {product.price}
-              </p>
+              <div className="relative w-full aspect-square">
+                <Image
+                  src={`${process.env.NEXT_PUBLIC_IMAGE_API_URL}${product.image}`}
+                  alt={product.name}
+                  fill
+                  sizes="100%"
+                  className="object-cover rounded-lg"
+                  priority
+                />
+              </div>
+
+              <div className="mt-2 text-center space-y-1">
+                <p className="text-xs bg-yellow-300 inline-block px-1 rounded">
+                  {product.code}
+                </p>
+                <p className="text-xs text-red-500 truncate">
+                  {product.description}
+                </p>
+                <p className="font-medium bg-yellow-300 inline-block px-1 rounded">
+                  R$ {product.price}
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
