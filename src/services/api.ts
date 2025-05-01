e4115dc2a5d4ecad03461cdbc395ce5dd1118416
Vitor <vitorantunes2003@gmail.com>
2025-05-01 11:22:24 -0300

    feat: add authorization token to API request headers for authenticated requests

--- src/services/api.ts
+++ src/services/api.ts
@@ -18,10 +18,19 @@ export interface Product {
   image: string;
 };
 
-const api = axios.create({ baseURL: API_BASE_URL });
+const api = axios.create({ 
+  baseURL: API_BASE_URL, 
+  headers: {
+    "Content-Type": "application/json"
+  },
+});
 
 api.interceptors.request.use(
   (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
     return config;
   },
   (error) => {
